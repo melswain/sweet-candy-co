43f0bde13f80089fb86c3600a46b3b787d75ec70
melswain <melanie.l.swain@gmail.com>
2025-12-02 13:19:54 -0500

    Working without gpio

--- Services/fan_service.py
+++ Services/fan_service.py
@@ -1,19 +1,19 @@
 # services/fan_service.py
-import RPi.GPIO as GPIO
-from time import sleep
+# import RPi.GPIO as GPIO
+# from time import sleep
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setwarnings(False)
 
 Motor1 = 22 # Enable Pin
 Motor2 = 27 # Input Pin
-GPIO.setup(Motor1,GPIO.OUT)
-GPIO.setup(Motor2,GPIO.OUT)
+# GPIO.setup(Motor1,GPIO.OUT)
+# GPIO.setup(Motor2,GPIO.OUT)
 
 def turnOnFan():
     try:
-        GPIO.output(Motor1,GPIO.HIGH)
-        GPIO.output(Motor2,GPIO.HIGH)
+        # GPIO.output(Motor1,GPIO.HIGH)
+        # GPIO.output(Motor2,GPIO.HIGH)
         print("Motor is on")
         return True,"Fan is ON"
     except Exception as e:
@@ -23,7 +23,7 @@ def turnOnFan():
 
 def turnOffFan():
     try:
-        GPIO.output(Motor1,GPIO.LOW)
+        # GPIO.output(Motor1,GPIO.LOW)
         print("Motor is off")
         return False,"Fan is OFF"
     except Exception as e:
