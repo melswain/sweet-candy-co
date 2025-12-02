f975c108d92ad2fbe4f9bba54ec002a807d50cf3
Kishaan <kishaan2006@gmail.com>
2025-12-01 21:59:23 -0500

    updating phase 2 email

--- Services/fan_service.py
+++ Services/fan_service.py
@@ -1,19 +1,19 @@
 # services/fan_service.py
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 from time import sleep
 
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 Motor1 = 22 # Enable Pin
 Motor2 = 27 # Input Pin
-# GPIO.setup(Motor1,GPIO.OUT)
-# GPIO.setup(Motor2,GPIO.OUT)
+GPIO.setup(Motor1,GPIO.OUT)
+GPIO.setup(Motor2,GPIO.OUT)
 
 def turnOnFan():
     try:
-        # GPIO.output(Motor1,GPIO.HIGH)
-        # GPIO.output(Motor2,GPIO.HIGH)
+        GPIO.output(Motor1,GPIO.HIGH)
+        GPIO.output(Motor2,GPIO.HIGH)
         print("Motor is on")
         return True,"Fan is ON"
     except Exception as e:
@@ -23,7 +23,8 @@ def turnOnFan():
 
 def turnOffFan():
     try:
-        # GPIO.output(Motor1,GPIO.LOW)
+        GPIO.output(Motor1,GPIO.LOW)
+        GPIO.output(Motor2,GPIO.LOW)
         print("Motor is off")
         return False,"Fan is OFF"
     except Exception as e:
@@ -32,7 +33,7 @@ def turnOffFan():
         print('Cleaning up...')
         #GPIO.cleanup()
 
-turnOffFan()
+# turnOffFan()
 
 def toggle_fan(enabled: bool):
     if enabled:
@@ -42,3 +43,4 @@ def toggle_fan(enabled: bool):
 
     print(f"Switch is now {'ON' if enabled else 'OFF'}")
     return success, message
+
